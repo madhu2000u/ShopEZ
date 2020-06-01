77d6403ada86539e90eea8c1b9dce858ad234121
madhu2000u <49570614+madhu2000u@users.noreply.github.com>
2020-06-01 20:33:31 +0530

    Add files via upload

---
+++ homepg_form_validation.js
@@ -0,0 +1,16 @@
+﻿$(document).ready(function(){
+	$("#login_form").validate({
+		rules:{
+			username: {required: true,  minlength: 9, number: true},
+			password: {required: true, minlength: 5}
+		},
+		messages:{
+			username: {required: "This field is required", minlength: "Must be 9 characters long", number: "Field must be a number"},
+			password: {required: "This field is required", minlength: "Must be atleast 5 characters long"}
+			
+		},
+		submitHandler: function(form){
+			form.submit();
+		}
+});
+});
